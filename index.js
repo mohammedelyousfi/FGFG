ca266d07e337a2d6c09cf544255dba2f648b826e
simomed12 <103375272+simomed12@users.noreply.github.com>
2022-04-10 17:48:52 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,80 @@
+/**
+ * if there is a saved game in localStorage, print the button #loadSavedGameB
+ */
+if(window.localStorage.getItem('backup') !== null && JSON.parse(window.localStorage.getItem('backup')) != null) {
+    document.getElementById("loadSavedGameB").style.display = "block";
+}
+
+
+/**
+ * set the localStorage variable 'loadSavedGame' to false in order to alert that the player wants to start a new game
+ * after that launch the html game page
+ */
+function newGame() {
+    if(window.localStorage.getItem('backup') !== null && JSON.parse(window.localStorage.getItem('backup')) != null) {
+        let bool =confirm("Si vous démarrez une nouvelle partie, vous perdrez votre sauvegarde précédente.");
+        if (bool == true) { 
+            window.localStorage.setItem('loadSavedGame', 'false');
+            window.location.href='../html/game.html';
+        }
+    }
+    else {
+        window.localStorage.setItem('loadSavedGame', 'false');
+        window.location.href='../html/game.html';
+    }
+}
+
+/**
+ * set the localStorage variable 'loadSavedGame' to true in order to alert that the player wants to load the saved game
+ * after that launch the html game page
+ */
+function loadSavedGame() {
+    if(window.localStorage.getItem('backup') !== null && JSON.parse(window.localStorage.getItem('backup')) != null) 
+    {
+        window.localStorage.setItem('loadSavedGame', 'true');
+        window.location.href='html/game.html';    
+    }
+    else { alert("Impossible de charger une partie... Commencez d'abord une nouvelle partie !"); }
+}
+
+
+
+
+/**
+ * attach the newGame function to the button #newGameB
+ */
+document.getElementById("newGameB").addEventListener("click", newGame);
+
+/**
+ * attach the loadSavedGame function to the button #loadSavedGameB
+ */
+document.getElementById("loadSavedGameB").addEventListener("click", loadSavedGame);
+
+/**
+ * open the levels_management page when click on the button #levelsManagementB
+ */
+document.getElementById("levelsManagementB").addEventListener("click", () => { window.location.href='html/levels_management.html'; });
+
+/**
+ * attach the function of to the button #volume
+ */
+document.getElementById("volume").addEventListener("click", () => { 
+    let audio = document.getElementById('audio');
+    if(audio.duration > 0 && !audio.paused) { 
+        audio.muted = !audio.muted;
+        if(audio.muted == true) { window.sessionStorage.setItem('muted', 'true'); }
+        else { window.sessionStorage.setItem('muted', 'false'); }
+    }
+    else { 
+        audio.play();
+        window.sessionStorage.setItem('muted', 'false');
+    }
+ });
+
+/**
+ * play or not the background music
+ */
+window.addEventListener("load", () => {
+    document.getElementById('audio').volume = 0.2;
+    if(window.sessionStorage.getItem('muted') == 'true') { document.getElementById('audio').muted = true; }
+});
